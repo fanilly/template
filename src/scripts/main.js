6d261121fa23d4eab58f318f1aa594787fa46aab
fanilly <815969707@qq.com>
2017-09-09 12:21:15 +0800

    del some file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,23 +3,3 @@
  * @DateTime:    2017-08-11 08:43:13
  * @Description: Description
  */
-//es6 新增了块级作用域
-{
-  const INFO = {
-    name:'allenAugustine',
-    address:'china'
-  };
-
-  /**
-   *
-   * es6的模板字符串
-   *
-   */
-  let str = `Hi ! My name is ${INFO.name}, I'm from ${INFO.address}`;
-  console.log(str);
-}
-
-{
-  let str = 'string';
-  console.log(str.includes('t'));
-}
